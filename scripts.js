7e42ebd013993c9141232f995cc165b4e8d5cca0
KajaGrzybowska <kajagrzybowska@outlook.com>
2017-06-07 16:44:11 +0200

    pole trojkata w js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+ 
+ var a = prompt('Podstawa trójkąta ma długość... ');
+ var h = prompt('Wysokość trójkąta ma długość... ');
+ var triangularArea = alert ('Pole trójkąta wynosi... ' + a*h/2 + ' cm2')
+ console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea)
+ 
+ 
