f7ef44c69d479bb7067abe0e5ca24aad99f0f71e
KajaGrzybowska <kajagrzybowska@outlook.com>
2017-06-12 15:27:11 +0200

    undefined-problem + średniki

--- scripts.js
+++ scripts.js
@@ -2,7 +2,8 @@
  
  var a = prompt('Podstawa trójkąta ma długość…');
  var h = prompt('Wysokość trójkąta ma długość…');
- var triangleArea = alert ('Pole trójkąta wynosi... ' + a * h / 2 + ' cm2')
- console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea)
+ var triangleArea = a * h / 2
+ alert('Pole trójkąta wynosi... ' + triangleArea);
+ console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
  
  
